5b28cdb5a5e32721b32a99bdf33b583b3278bcf8
amahmoudi <amahmoudi@PC100304.axones.fr>
2017-03-07 14:49:21 +0100

    reconfigure transaction manager

--- src/main/java/com/bakInfo/configuration/AppConfiguration.java
+++ src/main/java/com/bakInfo/configuration/AppConfiguration.java
@@ -4,20 +4,20 @@ import java.util.Properties;
 
 import javax.sql.DataSource;
 
-import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.hibernate4.HibernateTemplate;
-import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
 
 /**
  * 
@@ -28,7 +28,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @ComponentScan({ "com.bakInfo" })
 @PropertySource(value = { "classpath:application.properties" })
-public class AppConfiguration  {
+public class AppConfiguration  implements TransactionManagementConfigurer{
 	@Autowired
     private Environment environment;
  
@@ -61,19 +61,14 @@ public class AppConfiguration  {
     }
      
     @Bean
-    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
-        return new HibernateTransactionManager(sessionFactory);
-    }
-
-    @Bean
-    public HibernateTemplate hibernateTemplate(SessionFactory sessionFactory) {
-        return new HibernateTemplate(sessionFactory);
-    }
+    public PlatformTransactionManager platformTransactionManager(){ 
+    	return new DataSourceTransactionManager(dataSource());
+   }
     
-//    @Bean
-//	public PlatformTransactionManager transactionManager() {
-//		return new DataSourceTransactionManager(dataSource());
-//	}
+    @Override
+    public PlatformTransactionManager annotationDrivenTransactionManager() {
+        return platformTransactionManager();
+    }
     
     @Bean
     public PasswordEncoder passwordEncoder() {
